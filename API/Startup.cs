a0e9eb93dbb8d8e43df5fcc70bf321d094acbd47
Microsoft Learn Student <learn@contoso.com>
2021-10-13 12:00:15 +0100

    first commit

---
+++ API/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Persistance;
+
+
+namespace API
+{
+    public class Startup
+    {
+        public IConfiguration _config { get; }
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+         
+            
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+
+            services.AddDbContext<DataContext>(opt =>
+            {
+               
+                opt.UseMySql(_config.GetConnectionString("DefaultConnection"),serverVersion);
+               //opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+
+            //  app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
